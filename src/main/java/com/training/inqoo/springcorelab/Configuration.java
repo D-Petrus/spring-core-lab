34628fc38a8e73730adec7d573c4b2e97a72320f
PES34 <4headx@gmail.com>
2021-04-25 12:13:39 +0200

    add 4 beans

---
+++ src/main/java/com/training/inqoo/springcorelab/Configuration.java
@@ -0,0 +1,9 @@
+package com.training.inqoo.springcorelab;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Configuration {
+
+
+}
---
+++ src/main/java/com/training/inqoo/springcorelab/package1/DamianBean.java
@@ -0,0 +1,13 @@
+package com.training.inqoo.springcorelab.package1;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DamianBean {
+
+
+
+}
+
+
